d9c6ba13e642adc256a90442b22d52c7c0ca0467
Stanislas Ormières <stan@stormier.ninja>
2024-03-25 11:42:54 +0100

    chore: 🔧 build in ESM
    - problem: path aliases are not working with build

--- src/app.config.ts
+++ src/app.config.ts
@@ -5,7 +5,7 @@ import { playground } from "@colyseus/playground";
 /**
  * Import your Room files
  */
-import { MyRoom } from "./rooms/MyRoom";
+import { MyRoom } from "@/rooms/MyRoom.js";
 
 export default config({
 
